3a7af123ca6afd51b28964879fb208cc922e6f89
MariaHoulihan <mariahoulihan@gmail.com>
2023-02-22 16:29:31 +0000

    Add style amendments to back to top nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
+/* Reference to use of https://www.youtube.com/watch?v=C7NsIRhoWuE&list=LL&index=3 Kevin Briggs' code */
 function checkQuestions() {
-    var question1 = document.quiz.question1.value;
-    var question2 = document.quiz.question2.value;
-    var question3 = document.quiz.question3.value;
-    var question4 = document.quiz.question4.value;
-    var question5 = document.quiz.question5.value;
-    var question6 = document.quiz.question6.value;
-    var question7 = document.quiz.question7.value;
-    var question8 = document.quiz.question8.value;
-    var question9 = document.quiz.question9.value;
-    var question10 = document.quiz.question10.value;
+    const question1 = document.quiz.question1.value;
+    const question2 = document.quiz.question2.value;
+    const question3 = document.quiz.question3.value;
+    const question4 = document.quiz.question4.value;
+    const question5 = document.quiz.question5.value;
+    const question6 = document.quiz.question6.value;
+    const question7 = document.quiz.question7.value;
+    const question8 = document.quiz.question8.value;
+    const question9 = document.quiz.question9.value;
+    const question10 = document.quiz.question10.value;
 
     var correct = 0;
     var incorrect = 0;
@@ -45,9 +46,9 @@ function checkQuestions() {
     }
 
 var scoreMessage = [
-    "Excellent score! :D", 
+    "Excellent score! Way to go!! :D", 
     "Good score. Keep up the good work :)", 
-    "Awwww ... Hit the books to improve your score!"
+    "Awwww ... Hit the books to improve your score! You can do it :)"
 ];
 
 var images = [
@@ -73,5 +74,4 @@ document.getElementById("submit-area").style.visibility = "visible";
 document.getElementById("score-tally").innerHTML = "Your score is " + correct +" correct out of 10.";
 document.getElementById("score-message").innerHTML = scoreMessage[scoreTallyUp];
 document.getElementById("result-image").src = images[scoreTallyUp];
-
 }
